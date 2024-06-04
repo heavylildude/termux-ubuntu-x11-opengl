b88d8ed70ed621fc8bfe64303f90f18df600c167
HLD <heavylildude@gmail.com>
2024-06-04 19:02:01 +0700

    Add files via upload

---
+++ startme.sh
@@ -0,0 +1,3 @@
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &
+proot-distro login --user chris ubuntu --shared-tmp
